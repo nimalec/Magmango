1cb43f3480846c776f368ebe232f4c196e425e6a
nimlaec <47722154+nimalec@users.noreply.github.com>
2021-05-14 11:36:33 -0400

    write method for runscript implemented

--- magmango/calculation/runscript.py
+++ magmango/calculation/runscript.py
@@ -6,36 +6,24 @@ class RunscriptSettings:
       self._modules = modules
       self._exports = exports
       self._execute  = execute
-	  self._runscript_settings = {"run_settings": self._run_settings, "modules": self._modules, "exports": self._exports, "execute": self._execute}
+	  self._links  = links
+	  self._settings = {"run_settings": self._run_settings, "modules": self._modules, "exports": self._exports, "execute": self._execute, "links": self._links}
 		#self._run_settings  =  run_settings or {"job_name": "scf_calc" , "partition": "etna" , "account": "nimalec", "qos": , "tpn": , "time":  }
 		#self._modules = module_imports or ["module unload intel/2016.4.072", "module load intel/2018.5.274.par", "module load vasp_intelmpi/5.4.4.16052018"]
 		#self._run_exec = run_exec or {"exe": "'vasp_std'" , "run": "time mpirun $EXE"}
 		#self._links = links
 
    def runscript_from_file(self, file_path):
-      settings = read_runscript(settings, file_path)
-	  self._run_settings = settings["run_settings"]
-	  self._modules = settings["modules"]
-	  self._run_exec = settings["run_exec"]
-	  self._links = settings["links"]
+      self._settings = read_runscript(file_path)
 
-	def get_settings(self):
+   def get_settings(self, setting_type):
 		"""Function  """
-		settings = {"run_settings": self._run_settings, "modules": self._modules, "run_exec": self._run_exec, "links": self._links}
-		return settings
+		return self._runscript_settings[setting_type]
 
-	def update_settings(self, setting_type, key, value):
+	def update_settings(self, setting_type, value):
 		"""Function  """
-		if setting_type is "run_settings":
-			self._run_settings[key] = value
-		elif setting_type is "modules":
-			self._modules[key] = value
-		elif setting_type is "run_exec":
-			self._run_exec[key] = value
-		elif setting_type is "links":
-			self._link[key]= value
+		self._settings[setting_type] = value
 
 	def write_file(self, file_path):
 		"""Function  """
-		settings = {"run_settings": self._run_settings, "modules": self._modules, "run_settings": self._run_settings, "links": self._links}
-		write_runscript(file_path, settings)
+	   write_runscript(file_path, self._run_settings)
