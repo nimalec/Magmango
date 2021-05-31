b3b999ee9dbe2d21f2e8efb6a5262af29a5756e1
nimlaec <47722154+nimalec@users.noreply.github.com>
2021-05-30 21:33:46 -0700

    changed spin implementation in ncl_calc

--- magmango/mae/mae_calc.py
+++ magmango/mae/mae_calc.py
@@ -5,7 +5,7 @@ from magmango.calculation.calculation import Calculation
 #from magmango.calculation.incar_default_settings import DefaultMagNCLParameters
 
 class MagneticAnisotropyFlow:
-    def __init__(self, work_dir, npoints, angle_range, cl_calculation,cl_dir=None):
+    def __init__(self, work_dir, npoints, angle_range, cl_calculation, nbands, cl_dir=None):
         self._work_dir = work_dir
         self._npoints = npoints
         # If calculation class for CL provided
@@ -48,12 +48,26 @@ class MagneticAnisotropyFlow:
         spins = SpinVectors(npoints=npoints, angle_range=angle_range)
         self._spins = spins._spin_axes.T
 
-        def setup_ncl_calc(spins,incar_cl, poscar, potcar, kpoints):
-            ncl_calc = []
+        def setup_ncl_calc(work_dir, spins,incar_cl,poscar, potcar,kpoints,runscript,nbands):
+            ncl_calcs = []
             incar_ncl = incar_cl
-            incar_ncl.pop("MAGMOM")
-            for spin in spins:
+            incar_ncl.remove_settings("MAGMOM")
+            incar_ncl.update_settings("ISPIN", 1)
+            incar_ncl.update_settings("LSORBIT", ".TRUE.")
+            incar_ncl.update_settings("LNONCOLLINEAR", ".TRUE.")
+            incar_ncl.update_settings("LPLANE", ".FALSE.")
+            incar_ncl.update_settings("NBANDS", str(2*nbands))
 
+            ncl_dir = work_dir + "/scf_ncl"
+            itr=0
+            for spin in spins:
+                itr+=1
+                incar_ncl.update_settings("SAXIS",spin)
+                calc_ncl =  Calculation(ncl_dir+"ncl_"+str(itr),incar_ncl,kpoints,poscar,potcar._outfile_path,runscript)
+                ncl_calcs.append(calc_ncl)
+            return ncl_calcs
+
+        self._ncl_calculations = setup_ncl_calc(work_dir, spins,incar_cl,poscar,potcar,kpoints,runscript,nbands)
 
             #kpoints = cl_calc.
 
